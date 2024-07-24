e3ce91b93cac226dfb8cdb6b5451721d419b78c0
Diego Lopez <diegolopezuncc@gmail.com>
2024-07-24 16:08:26 -0400

    wip: website base

---
+++ src/app/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Empowering retail market participants
+        </p>
+      </div>
+
+      <div className="relative flex place-items-center">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/newton_logo.svg"
+          alt="Newton Group Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <h2 className="mb-3 text-2xl font-semibold">
+            Equal Footing
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Providing retail investors with institutional-grade strategies.
+          </p>
+        </div>
+
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <h2 className="mb-3 text-2xl font-semibold">
+            Systematic Approach
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Reducing barriers to understanding and utilizing systematic trading strategies.
+          </p>
+        </div>
+
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <h2 className="mb-3 text-2xl font-semibold">
+            Innovative Platform
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Developing cutting-edge tools for retail market participants.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
